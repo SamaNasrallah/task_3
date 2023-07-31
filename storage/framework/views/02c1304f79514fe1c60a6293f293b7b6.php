63054cd201ae6ba5551d664eda4cf82741e7d680
SamaNasrallah <93013128+SamaNasrallah@users.noreply.github.com>
2023-07-31 11:15:27 +0300

    Add files via upload

---
+++ storage/framework/views/02c1304f79514fe1c60a6293f293b7b6.php
@@ -0,0 +1,19 @@
+
+<?php $__env->startSection('MainContent'); ?>
+
+
+
+
+<div class="row form-container">
+    <div class="col-md-12" >
+        <h2>Course Details <?php echo e($course->category); ?>  :</h2>
+        <?php echo $course->details; ?>
+
+    </div>
+
+</div>
+
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\MI\Desktop\laravel\task3\resources\views/admin/details.blade.php ENDPATH**/ ?>
